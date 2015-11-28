9b70f0c1355abff729c3fcab63b3cc4d7671d078
John Evans <lgastako@gmail.com>
2015-11-28 03:48:16 -0500

    Divergence.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject zen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :description "The Zen programming language."
+  :url "http://github.com/lgastako/zen"
+  :dependencies [[instaparse "1.4.1"]
+                 [org.clojure/clojure "1.7.0"]])
--- src/zen/core.clj
+++ src/zen/core.clj
@@ -1,6 +1,24 @@
-(ns zen.core)
+(ns zen.core
+  (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def parse
+  (insta/parser
+   "NAME = ...
+    EXPR = FUNC | NAME | APPL
+    FUNC = λ NAME . EXPR"))
+
+;; (def as-and-bs
+;;   (insta/parser
+;;    "S = AB*
+;;     AB = A B
+;;     A = 'a'+
+;;     B = 'b'+"))
+
+;; ;; => (as-and-bs "aaaaabbbaaaabb")
+;; ;; [:S
+;; ;;  [:AB [:A "a" "a" "a" "a" "a"] [:B "b" "b" "b"]]
+;; ;;  [:AB [:A "a" "a" "a" "a"] [:B "b" "b"]]]
+
+;; [:S
+;;  [:AB [:A "a" "a" "a" "a" "a"] [:B "b" "b" "b"]]
+;;   [:AB [:A "a" "a" "a" "a"] [:B "b" "b"]]
